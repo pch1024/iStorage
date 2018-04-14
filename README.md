b5bb762ce7bc7dc6ac430777161c02c0d52fe383
DESKTOP-2RVJAMA\PCH10 <pch1024@outlook.com>
2018-04-15 00:28:20 +0800

    新增测试代码

--- README.md
+++ README.md
@@ -1,41 +1,87 @@
 <h1 align="center">localStorage</h1>
+<p align="center">:ghost::ghost::ghost::ghost::ghost:</p>
 
-# iStorage  是什么
+# iStorage 是什么
 
-iStorage 是针对浏览器 localStorage 的一个便捷操作插件。它支持用户直接传值（支持直接存储数字(非NaN)、字符串、数组、JSON类型），并且直接返回同样的值。
-iStorage 是基于原生 JS 实现的，不依赖任何框架。它编译压缩后是 1.77kb，是一款非常轻量的 JS lib。
-作者的第一个NPM插件，不足之处还请见谅和指正。
+iStorage 是针对浏览器 localStorage 的一个便捷操作插件。它支持用户直接传值（支持直接存储数字(非 NaN)、字符串、数组、JSON 类型），并且直接返回同样的值。
+iStorage 是基于原生 JS 实现的，不依赖任何框架。它编译压缩后是 1.77kb，是一款非常轻量的 JS lib。作者的第一个 NPM 插件，不足之处还请见谅和指正。
 
 # iStorage 怎么用
 
-iStorage 支持 `CommonJS` `RequireJS` `<script>` 方式引入。
-## npm 
+## iStorage 支持 `CommonJS` `RequireJS` `<script>` 方式引入。
+
+### npm
+
 ```js
-import iStorage from 'iStorage'
+import iStorage from "iStorage";
 ```
 
-## 其他
+### 其他
+
 ```html
 <script src="iStorage.min.js"></script>
 ```
 
-iStorage 支持 `Check` `Get` `Set` `Remove` 操作。`debug` 参数可选，用于在控制台打印操作详情日志。
+## iStorage 支持 `Check` `Get` `Set` `Remove` 操作。`debug` 参数可选，用于在控制台打印操作详情日志。
 
-## Check
+### Check
 
 ```js
-iStorage.check(key:String, debug:Boolean) 
-```
-## Get
-```js
-iStorage.get(key:String, debug:Boolean) 
+iStorage.check((key: String), (debug: Boolean));
 ```
 
-## Set
+### Get
+
 ```js
-iStorage.set(key:String, value:!NaN || Number || String || Array || JSON, debug:Boolean) 
+iStorage.get((key: String), (debug: Boolean));
 ```
-## Remove
+
+### Set
+
 ```js
-iStorage.remove(key:String, debug:Boolean) 
+iStorage.set(key:String, value:!NaN || Number || String || Array || JSON, debug:Boolean)
+```
+
+### Remove
+
+```js
+iStorage.remove((key: String), (debug: Boolean));
+```
+## Testing
+
+```js
+iStorage.get('testing',true);
+iStorage.check('testing',true);
+iStorage.remove('testing',true);
+
+iStorage.check('testing',1,true);
+iStorage.get('testing',1,true);
+iStorage.set('testing',1,true);
+iStorage.remove('testing',1,true);
+
+iStorage.remove('testing',true);
+iStorage.set('testing',[],true);
+iStorage.set('testing',[1],true);
+iStorage.get('testing',true);
+iStorage.check('testing',true);
+
+iStorage.set('testing',"",true);
+iStorage.set('testing',"1",true);
+iStorage.check('testing',true);
+iStorage.remove('testing',true);
+iStorage.get('testing',true);
+
+iStorage.set('testing',{a:1},true);
+iStorage.set('testing',{},true);
+
+error
+iStorage.set('');
+iStorage.set('testing');
+iStorage.set('testing',true);
+
+
+console.log( iStorage.set('testing',undefined) )
+console.log( iStorage.set('testing',null) )
+console.log( iStorage.set('testing',NaN) )
+
 ```
--- iStorage.js
+++ iStorage.js
@@ -1,130 +1,121 @@
-(function (definition) {
-    "use strict";
-    // CommonJS
-    if (typeof exports === "object" && typeof module === "object") {
-        module.exports = definition();
-        // RequireJS
-    } else if (typeof define === "function" && define.amd) {
-        define(definition);
-        // <script>
-    } else if (typeof window !== "undefined" || typeof self !== "undefined") {
-        // Prefer window over self for add-on scripts. Use self for
-        // non-windowed contexts.
-        var global = typeof window !== "undefined" ? window : self;
+(function(definition) {
+  "use strict";
+  // CommonJS
+  if (typeof exports === "object" && typeof module === "object") {
+    module.exports = definition();
+    // RequireJS
+  } else if (typeof define === "function" && define.amd) {
+    define(definition);
+    // <script>
+  } else if (typeof window !== "undefined" || typeof self !== "undefined") {
+    // Prefer window over self for add-on scripts. Use self for
+    // non-windowed contexts.
+    var global = typeof window !== "undefined" ? window : self;
 
-        // initialize iStorage as a global.
-        global.iStorage = definition();
+    // initialize iStorage as a global.
+    global.iStorage = definition();
+  } else {
+    throw new Error(
+      "This environment was not anticipated by iStorage,Please file a bug."
+    );
+  }
+})(function() {
+  /**
+   * 构造组合原型函数，伪经典继承
+   */
+  function IStorage() {
+    this.data = {
+      iStorage: null
+    };
+    this.tip =
+      "value only support Number(not include NaN)、String、Array、JSON ！But you gave：";
+  }
 
+  /**
+   * 值类型检测并处理转化成JSON字符串
+   * @param {String} value
+   */
+  IStorage.prototype.handlerValue = function(value) {
+    var type = Object.prototype.toString.call(value);
+    switch (type) {
+      case "[object Number]":
+        if (isNaN(value)) throw this.tip + "NaN";
+        this.data.iStorage = value;
+        break;
+      case "[object String]":
+        this.data.iStorage = value;
+        break;
+      case "[object Array]":
+        this.data.iStorage = value;
+        break;
+      case "[object Object]":
+        this.data.iStorage = value;
+        break;
+      default:
+        throw this.tip + type;
+    }
+    return JSON.stringify(this.data);
+  };
+
+  /**
+   * 键名检查
+   * @param {String} key
+   * @param {Boolean} debug
+   */
+  IStorage.prototype.check = function(key, debug) {
+    if (debug) {
+      if (localStorage.getItem(key)) {
+        console.log("check success: " + key + localStorage.getItem(key));
+      } else {
+        console.log("check failure: not find " + key);
+      }
+    }
+    return localStorage.getItem(key) ? true : false;
+  };
+
+  /**
+   * 依据键名获取值
+   * @param {String} key
+   * @param {Boolean} debug
+   */
+  IStorage.prototype.get = function(key, debug) {
+    if (!this.check(key)) {
+      if (debug) console.log("get failure:" + key);
+      return localStorage.getItem(key);
     } else {
-        throw new Error("This environment was not anticipated by iStorage,Please file a bug.");
+      if (debug) console.log("get success:" + key + localStorage.getItem(key));
+      return JSON.parse(localStorage.getItem(key)).iStorage;
     }
-})(function () {
+  };
 
-    /**
-     * 构造组合原型函数，伪经典继承
-     */
-    function IStorage() {
-        this.data = {
-            "iStorage": null
-        }
-        this.tip = "value only support Number(Do not include NaN)、String、Array、JSON ！But you gave ：";
+  /**
+   * 添加键值对,值类型允许长度为0
+   * @param {String} key
+   * @param {!NaN||Number||String||Array||JSON} value
+   * @param {Boolean} debug
+   */
+  IStorage.prototype.set = function(key, value, debug) {
+    value = this.handlerValue(value);
+    localStorage.setItem(key, value);
+    if (debug) console.log("set success: " + key + value);
+    return this.get(key);
+  };
+
+  /**
+   * 删除键值对
+   * @param {String} key
+   * @param {Boolean} debug
+   */
+  IStorage.prototype.remove = function(key, debug) {
+    if (debug && this.check(key)) {
+      console.log("remove success: " + key + this.get(key));
+    } else {
+      console.log("remove failure: not find " + key);
     }
+    localStorage.removeItem(key);
+    return this.get(key);
+  };
 
-    /**
-     * 值类型检测并处理转化成JSON字符串
-     * @param {String} value 
-     */
-    IStorage.prototype.handlerValue = function (value) {
-        var type = Object.prototype.toString.call(value)
-        switch (type) {
-            case '[object Number]':
-                if (isNaN(value)) {
-                    throw this.tip + "NaN";
-                } else {
-                    this.data.iStorage = value;
-                    return JSON.stringify(this.data);
-                }
-                break;
-            case '[object String]':
-                this.data.iStorage = value;
-                return JSON.stringify(this.data);
-                break;
-            case '[object Array]':
-                this.data.iStorage = value;
-                return JSON.stringify(this.data)
-                break;
-            case '[object Object]':
-                this.data.iStorage = value;
-                return JSON.stringify(this.data)
-                break;
-            default:
-                throw this.tip + type;
-                break;
-        }
-
-    }
-
-    /**
-     * 键名检查
-     * @param {String} key 
-     * @param {Boolean} debug 
-     */
-    IStorage.prototype.check = function (key, debug) {
-        if (debug) {
-            if (localStorage.getItem(key)) {
-                console.log("check success: " + key + localStorage.getItem(key));
-            } else {
-                console.log("check failure: not find " + key);
-            }
-        }
-        return localStorage.getItem(key) ? true : false;
-    }
-
-    /**
-     * 依据键名获取值
-     * @param {String} key 
-     * @param {Boolean} debug 
-     */
-    IStorage.prototype.get = function (key, debug) {
-        if (!this.check(key)) {
-            if (debug) console.log("get failure:" + key);
-            return localStorage.getItem(key);
-        } else {
-            if (debug) console.log("get success:" + key + localStorage.getItem(key));
-            return JSON.parse(localStorage.getItem(key)).iStorage;
-        }
-    }
-
-    /**
-     * 添加键值对,值类型允许长度为0
-     * @param {String} key 
-     * @param {!NaN||Number||String||Array||JSON} value 
-     * @param {Boolean} debug 
-     */
-    IStorage.prototype.set = function (key, value, debug) {
-        value = this.handlerValue(value);
-        localStorage.setItem(key, value);
-        if (debug) console.log("set success: " + key + value);
-        return this.get(key);
-    }
-
-    /**
-     * 删除键值对
-     * @param {String} key 
-     * @param {Boolean} debug 
-     */
-    IStorage.prototype.remove = function (key, debug) {
-        if (debug) {
-            if (this.check(key)) {
-                console.log("remove success: " + key + this.get(key));
-            } else {
-                console.log("remove failure: not find " + key);
-            }
-        }
-        localStorage.removeItem(key);
-        return this.get(key);
-    }
-
-    var iStorage = new IStorage();
-    return iStorage;
-})
+  var iStorage = new IStorage();
+  return iStorage;
+});
